210149cb244ca773553e3e991ee5f74bd8f65a3d
Vladislav Manchev <v@bin.bz>
2019-07-24 17:47:06 +0200

    Simplifying contract and tests, addressing issues

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -6,38 +6,23 @@ import "./Pausable.sol";
 contract Remittance is Pausable {
     using SafeMath for uint256;
 
-    uint256 paymentId;
     uint256 public deadlineDelta;
     uint32 public commission;
 
-    enum Status { Pending, Claimed, Refunded }
     struct Payment {
-        uint256 id;
-        Status status;
         uint256 amount;
         address payable payer;
-        address payable intermediary;
-        bytes32 payeePassword;
-        bytes32 intermediaryPassword;
-        uint256 created;
         uint256 expires;
     }
     mapping (bytes32 => Payment) public payments;
 
-    event LogPasswordGenerated(address indexed payer);
-    event LogPasswordChecked(address indexed intermediary);
-    event LogContractKilled(address indexed killer);
     event LogDeposited(address indexed payer, uint256 amount);
-    event LogClaimed(address indexed intermediary, address indexed payer, uint256 amount);
+    event LogClaimed(address indexed intermediary, uint256 amount);
     event LogRefunded(address indexed payer, uint256 amount);
 
     constructor(uint256 _deadlineDelta, uint32 _commission, bool _paused) Pausable(_paused) public {
         deadlineDelta = _deadlineDelta;
         commission = _commission;
-    }
-
-    function getStatus(bytes32 paymentHash) public view returns (Status) {
-        return payments[paymentHash].status;
     }
 
     function changeDeadlineDelta(uint256 newDeadlineDelta) external onlyOwner {
@@ -54,122 +39,62 @@ contract Remittance is Pausable {
     }
 
     function deposit(
-        address payable intermediary,
-        bytes32 payeePassword,
-        bytes32 intermediaryPassword,
-        uint256 expires
+        address intermediary,
+        bytes32 password,
+        uint32 expires
     ) external payable whenAlive whenRunning coversCommission returns (bytes32) {
-        uint256 id = paymentId++;
-        bytes32 paymentHash = keccak256(
+        bytes32 id = keccak256(
             abi.encode(
-                id,
-                msg.value - commission,
-                msg.sender,
-                intermediary,
-                payeePassword,
-                intermediaryPassword,
-                now,
-                expires
+                password,
+                intermediary
             )
         );
-        payments[paymentHash] = Payment(
-            id,
-            Status.Pending,
+
+        require(payments[id].amount == 0, 'Duplicate payment');
+
+        payments[id] = Payment(
             msg.value - commission,
             msg.sender,
-            intermediary,
-            payeePassword,
-            intermediaryPassword,
-            now,
-            expires
+            now + expires
         );
 
-        return paymentHash;
-    }
+        emit LogDeposited(msg.sender, msg.value);
 
-    modifier onlyPayer(bytes32 paymentHash) {
-        require(msg.sender == payments[paymentHash].payer, 'Only payer allowed');
-        _;
-    }
-
-    modifier onlyPending(bytes32 paymentHash) {
-        require(
-            payments[paymentHash].status == Status.Pending,
-            'Cannot make changes at this point'
-        );
-        _;
-    }
-
-    function changePayeePassword(
-        bytes32 paymentHash,
-        bytes32 newPassword
-    ) external whenAlive onlyPayer(paymentHash) onlyPending(paymentHash) {
-        payments[paymentHash].payeePassword = newPassword;
-    }
-
-    function changeIntermediaryPassword(
-        bytes32 paymentHash,
-        bytes32 newPassword
-    ) external whenAlive onlyPayer(paymentHash) onlyPending(paymentHash) {
-        payments[paymentHash].intermediaryPassword = newPassword;
-    }
-
-    modifier onlyIntermediary(bytes32 paymentHash) {
-        require(msg.sender == payments[paymentHash].intermediary, 'Only intermediary allowed');
-        _;
-    }
-
-    modifier passwordsMatch(
-        bytes32 paymentHash,
-        bytes32 payeePassword,
-        bytes32 intermediaryPassword
-    ) {
-        Payment memory payment = payments[paymentHash];
-
-        bytes32 payeeHash = keccak256(abi.encode(payeePassword));
-        require(payment.payeePassword == payeeHash, 'Payee password does not match!');
-
-        bytes32 intermediaryHash = keccak256(abi.encode(intermediaryPassword));
-        require(
-            payment.intermediaryPassword == intermediaryHash,
-            'Intermediary password does not match!'
-        );
-        _;
+        return id;
     }
 
     function claim(
-        bytes32 paymentHash,
-        bytes32 payeePassword,
-        bytes32 intermediaryPassword
-    ) external whenAlive onlyIntermediary(paymentHash) passwordsMatch(paymentHash, payeePassword, intermediaryPassword) {
-        Payment memory payment = payments[paymentHash];
-        payments[paymentHash].status = Status.Claimed;
-
-        emit LogClaimed(msg.sender, payment.payer, payment.amount);
-
-        payment.intermediary.transfer(payment.amount);
-    }
-
-    modifier onlyAfterContractExpiry(bytes32 paymentHash) {
-        Payment memory payment = payments[paymentHash];
-        require(payment.created.add(payment.expires) < now, 'Contract has not yet expired');
-        _;
-    }
-
-    modifier onlyBeforeDeadline(bytes32 paymentHash) {
-        Payment memory payment = payments[paymentHash];
-        require(
-            payment.created.add(payment.expires).add(deadlineDelta) > now,
-            'Refund deadline has already passed'
+        bytes32 password
+    ) external whenAlive {
+        bytes32 id = keccak256(
+            abi.encode(
+                password,
+                msg.sender
+            )
         );
-        _;
+
+        Payment memory payment = payments[id];
+
+        require(payment.amount > 0, 'Wrong credentials or deposit already claimed');
+        require(payment.expires > now, 'Deposit already expired');
+
+        delete payments[id];
+
+        emit LogClaimed(msg.sender, payment.amount);
+
+        msg.sender.transfer(payment.amount);
     }
 
     function refund(
-        bytes32 paymentHash
-    ) external whenAlive onlyPayer(paymentHash) onlyAfterContractExpiry(paymentHash) onlyBeforeDeadline(paymentHash) {
-        Payment memory payment = payments[paymentHash];
-        payment.status = Status.Refunded;
+        bytes32 id
+    ) external whenAlive {
+        Payment memory payment = payments[id];
+
+        require(msg.sender == payment.payer, 'Only payer allowed');
+        require(payment.expires < now, 'Deposit has not yet expired');
+        require(payment.expires.add(deadlineDelta) > now, 'Refund deadline has already passed');
+
+        delete payments[id];
 
         emit LogRefunded(msg.sender, payment.amount);
 
--- test/remittance.js
+++ test/remittance.js
@@ -4,7 +4,22 @@ const Remittance = artifacts.require('./Remittance.sol');
 contract('Remittance', accounts => {
   let remittance;
   const [owner, payer1, intermediary1, payer2, intermediary2] = accounts;
-  const statuses = { Pending: new BN(0), Claimed: new BN(1), Refunded: new BN(2) };
+  const passwords1 = [
+    web3.utils.padRight('blablacio', 64),
+    web3.utils.padRight('oicalbalb', 64)
+  ];
+  const password1Hash = web3.utils.keccak256(passwords1.join(''));
+  const passwords2 = [
+    web3.utils.padRight('random', 64),
+    web3.utils.padRight('passwd', 64)
+  ];
+  const password2Hash = web3.utils.keccak256(passwords2.join(''));
+  const passwords3 = [
+    web3.utils.padRight('another', 64),
+    web3.utils.padRight('passwd', 64)
+  ];
+  const password3Hash = web3.utils.keccak256(passwords3.join(''));
+
 
   beforeEach('setup contract for each test', async () => {
     remittance = await Remittance.new(86400 * 14, 10000, false, { from: owner});
@@ -41,8 +56,7 @@ contract('Remittance', accounts => {
     try {
       await remittance.deposit(
         intermediary1,
-        '0xea1c426e4e705c5bd7883fa29ddd4579226a7627993ac2f85c7afa1df1a39d8c',
-        '0xc9bcf7287deeb04d4f3ac502fed09ccb2ee69988023843c52f7f59e69d6f9696',
+        password1Hash,
         86400 * 10,
         { from: payer1, value: 10000 }
       );
@@ -54,16 +68,14 @@ contract('Remittance', accounts => {
   it('should handle deposits properly', async() => {
     let paymentHash = await remittance.deposit.call(
       intermediary1,
-      '0xea1c426e4e705c5bd7883fa29ddd4579226a7627993ac2f85c7afa1df1a39d8c',
-      '0xc9bcf7287deeb04d4f3ac502fed09ccb2ee69988023843c52f7f59e69d6f9696',
+      password1Hash,
       86400 * 3,
       { from: payer1, value: 100000 }
     );
 
     await remittance.deposit(
       intermediary1,
-      '0xea1c426e4e705c5bd7883fa29ddd4579226a7627993ac2f85c7afa1df1a39d8c',
-      '0xc9bcf7287deeb04d4f3ac502fed09ccb2ee69988023843c52f7f59e69d6f9696',
+      password1Hash,
       86400 * 3,
       { from: payer1, value: 100000 }
     );
@@ -71,132 +83,47 @@ contract('Remittance', accounts => {
     let payment = await remittance.payments(paymentHash);
 
     assert.strictEqual(
-      payment.payeePassword,
-      '0xea1c426e4e705c5bd7883fa29ddd4579226a7627993ac2f85c7afa1df1a39d8c',
-      'Wrong payee password'
-    );
-    assert.strictEqual(
-      payment.intermediaryPassword,
-      '0xc9bcf7287deeb04d4f3ac502fed09ccb2ee69988023843c52f7f59e69d6f9696',
-      'Wrong intermediary password'
+      paymentHash,
+      '0xeaa149beaf640eb81d28ee1fddfd4b5f6b3532ee7ed9f9ea1e5ed16612ef4c22',
+      'Wrong payment hash'
     );
     assert.isTrue(
       payment.amount.eq(new BN(100000).sub(new BN(10000))),
       'Wrong amount'
     );
-    assert.isTrue(payment.status.eq(statuses.Pending), 'Wrong initial status');
     assert.strictEqual(payment.payer, payer1, 'Wrong payer');
-    assert.strictEqual(payment.intermediary, intermediary1, 'Wrong initial intermediary');
-    assert.isTrue(payment.expires.eq(new BN(86400).mul(new BN(3))), 'Wrong expiry');
   });
 
-  it('should only enable payer to change passwords', async() => {
-    let paymentHash = await remittance.deposit.call(
-      intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
-      86400 * 3,
-      { from: payer1, value: 100000 }
-    );
-
+  it('should enable only intermediaries to claim with correct password', async() => {
     await remittance.deposit(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
-      86400 * 3,
-      { from: payer1, value: 100000 }
-    );
-
-    try {
-      await remittance.changePayeePassword(
-        paymentHash,
-        '0xb10e2d527612073b26eecdfd717e6a320cf44b4afac2b0732d9fcbe2b7fa0cf6',
-        { from: payer2 }
-      );
-    } catch(err) {
-      assert.strictEqual(err.reason, 'Only payer allowed');
-    }
-
-    let payment = await remittance.payments(paymentHash);
-
-    assert.strictEqual(
-      payment.payeePassword,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489'
-    );
-
-    try {
-      await remittance.changeIntermediaryPassword(
-        paymentHash,
-        '0xb10e2d527612073b26eecdfd717e6a320cf44b4afac2b0732d9fcbe2b7fa0cf6',
-        { from: payer2 }
-      );
-    } catch(err) {
-      assert.strictEqual(err.reason, 'Only payer allowed');
-    }
-
-    payment = await remittance.payments(paymentHash);
-    assert.strictEqual(
-      payment.intermediaryPassword,
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61'
-    );
-  });
-
-  it('should enable only intermediaries to claim with proper passwords', async() => {
-    let paymentHash = await remittance.deposit.call(
-      intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
-      86400 * 3,
-      { from: payer1, value: 100000 }
-    );
-
-    let deposit = await remittance.deposit(
-      intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password1Hash,
       86400 * 3,
       { from: payer1, value: 100000 }
     );
 
     try {
       await remittance.claim(
-        paymentHash,
-        web3.utils.padRight(web3.utils.toHex('blablacio'), 64),
-        web3.utils.padRight(web3.utils.toHex('oicalbalb')),
+        password1Hash,
         { from: intermediary2 }
       );
     } catch(err) {
-      assert.strictEqual(err.reason, 'Only intermediary allowed');
+      assert.strictEqual(err.reason, 'Wrong credentials or deposit already claimed');
     }
     
     try {
       await remittance.claim(
-        paymentHash,
-        web3.utils.padRight(web3.utils.toHex('wrong'), 64),
-        web3.utils.padRight(web3.utils.toHex('oicalbalb'), 64),
+        password2Hash,
         { from: intermediary1 }
       );
     } catch(err) {
-      assert.strictEqual(err.reason, 'Payee password does not match!');
-    }
-
-    try {
-      await remittance.claim(
-        paymentHash,
-        web3.utils.padRight(web3.utils.toHex('blablacio'), 64),
-        web3.utils.padRight(web3.utils.toHex('wrong'), 64),
-        { from: intermediary1 }
-      );
-    } catch(err) {
-      assert.strictEqual(err.reason, 'Intermediary password does not match!');
+      assert.strictEqual(err.reason, 'Wrong credentials or deposit already claimed');
     }
 
     let intermediary1StartingBalance = new BN(await web3.eth.getBalance(intermediary1));
 
     let tx = await remittance.claim(
-      paymentHash,
-      web3.utils.padRight(web3.utils.toHex('blablacio'), 64),
-      web3.utils.padRight(web3.utils.toHex('oicalbalb'), 64),
+      password1Hash,
       { from: intermediary1, gasPrice: 42 }
     );
 
@@ -208,25 +135,19 @@ contract('Remittance', accounts => {
       .sub(new BN(tx.receipt.gasUsed).mul(new BN(42)))
       .eq(intermediary1EndingBalance)
     );
-
-    payment = await remittance.payments(paymentHash);
-
-    assert.isTrue(payment.status.eq(statuses.Claimed));
   });
 
   it('should enable only payer to get a refund within refund window', async() => {
     let paymentHash = await remittance.deposit.call(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password1Hash,
       86400 * 14,
       { from: payer1, value: 100000 }
     );
 
     await remittance.deposit(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password1Hash,
       86400 * 14,
       { from: payer1, value: 100000 }
     );
@@ -240,43 +161,25 @@ contract('Remittance', accounts => {
       assert.strictEqual(err.reason, 'Only payer allowed');
     }
 
-    paymentHash = await remittance.deposit.call(
-      intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
-      60,
-      { from: payer1, value: 100000 }
-    );
-
-    await remittance.deposit(
-      intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
-      60,
-      { from: payer1, value: 100000 }
-    );
-
     try {
       await remittance.refund(
         paymentHash,
         { from: payer1 }
       );
     } catch(err) {
-      assert.strictEqual(err.reason, 'Contract has not yet expired');
+      assert.strictEqual(err.reason, 'Deposit has not yet expired');
     }
 
     paymentHash = await remittance.deposit.call(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password2Hash,
       60,
       { from: payer1, value: 100000 }
     );
 
     await remittance.deposit(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password2Hash,
       60,
       { from: payer1, value: 100000 }
     );
@@ -301,16 +204,14 @@ contract('Remittance', accounts => {
 
     paymentHash = await remittance.deposit.call(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password3Hash,
       60,
       { from: payer1, value: 100000 }
     );
 
     await remittance.deposit(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password3Hash,
       60,
       { from: payer1, value: 100000 }
     );
@@ -340,6 +241,26 @@ contract('Remittance', accounts => {
       .sub(new BN(tx.receipt.gasUsed).mul(new BN(42)))
       .eq(payer1EndingBalance)
     );
+  });
+
+  it('should not allow duplicate deposits', async() => {
+    await remittance.deposit(
+      intermediary1,
+      password1Hash,
+      86400 * 14,
+      { from: payer1, value: 100000 }
+    );
+
+    try {
+      await remittance.deposit(
+        intermediary1,
+        password1Hash,
+        86400 * 14,
+        { from: payer1, value: 100000 }
+      );
+    } catch(err) {
+      assert.strictEqual(err.reason, 'Duplicate payment');
+    }
   });
 
   it('should only allow owner to pause, resume and kill', async() => {
@@ -401,8 +322,7 @@ contract('Remittance', accounts => {
     try {
       await remittance.deposit(
         intermediary1,
-        '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-        '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+        password1Hash,
         60,
         { from: payer1, value: 100000 }
       );
@@ -416,16 +336,14 @@ contract('Remittance', accounts => {
 
     paymentHash = await remittance.deposit.call(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password1Hash,
       60,
       { from: payer1, value: 100000 }
     );
 
     await remittance.deposit(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password1Hash,
       60,
       { from: payer1, value: 100000 }
     );
@@ -434,9 +352,7 @@ contract('Remittance', accounts => {
     let intermediary1StartingBalance = new BN(await web3.eth.getBalance(intermediary1));
 
     let tx = await remittance.claim(
-      paymentHash,
-      web3.utils.padRight(web3.utils.toHex('blablacio')),
-      web3.utils.padRight(web3.utils.toHex('oicalbalb')),
+      password1Hash,
       { from: intermediary1, gasPrice: 42 }
     );
 
@@ -455,23 +371,20 @@ contract('Remittance', accounts => {
 
     paymentHash = await remittance.deposit.call(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password1Hash,
       60,
       { from: payer1, value: 100000 }
     );
 
     await remittance.deposit(
       intermediary1,
-      '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-      '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+      password1Hash,
       60,
       { from: payer1, value: 100000 }
     );
 
     let payment = await remittance.payments(paymentHash);
     assert.strictEqual(payment.payer, payer1);
-    assert.strictEqual(payment.intermediary, intermediary1);
     assert.isTrue(payment.amount.add(new BN(10000)).eq(new BN(100000)));
 
     await remittance.pause({ from: owner });
@@ -480,8 +393,7 @@ contract('Remittance', accounts => {
     try {
       await remittance.deposit(
         intermediary1,
-        '0xae3230e7df366e086e69d35dc2a1b614fb48339756a72741c39654a78c4c9489',
-        '0x892a5c2516d3e850745f5788a81c679facc50c002f9326881f37106b02d85a61',
+        password1Hash,
         60,
         { from: payer1, value: 100000 }
       );
@@ -491,9 +403,7 @@ contract('Remittance', accounts => {
 
     try {
       await remittance.claim(
-        paymentHash,
-        web3.utils.padRight(web3.utils.toHex('blablacio')),
-        web3.utils.padRight(web3.utils.toHex('oicalbalb')),
+        password1Hash,
         { from: intermediary1 }
       );
     } catch(err) {
